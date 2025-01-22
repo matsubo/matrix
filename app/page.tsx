40e146fbb8c214a74f63ce733262094b3029b1b4
Yuki Matsukura <matsubokkuri@gmail.com>
2025-01-22 17:05:02 +0900

    set title

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import MatrixRain from "./components/MatrixRain"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="sr-only">Matrix Code</h1>
       <MatrixRain />
     </main>
   )
